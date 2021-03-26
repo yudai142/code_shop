6fb1b42cb4249481e5860d9e362d11ab3da5e66d
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-03-26 13:21:25 +0900

    データベース接続作成

---
+++ dbc.php
@@ -0,0 +1,23 @@
+<?php
+function dbc(){
+  $host = "localhost";
+  $dbname = "code_shop";
+  $user = "root";
+  $pass = "root";
+
+  $dns = "mysql:host=$host;dbname=$dbname;charset=utf8;";
+
+  try {
+    $pdo = new PDO($dns, $user, $pass, [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    // echo 'Mysql接続に成功しました';
+    // echo '<br>';
+    return $pdo;
+  } catch(PDOException $e) {
+    exit($e->getMessage());
+    echo '<br>';
+  }
+}
+?>
